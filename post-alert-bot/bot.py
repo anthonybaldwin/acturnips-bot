aa4eaa1327447aa1f0110864fb466b7307c2054b
Anthony Baldwin <anthony.baldwin@gmail.com>
2020-05-04 23:35:01 -0700

    mention fix

--- post-alert-bot/bot.py
+++ post-alert-bot/bot.py
@@ -38,7 +38,7 @@ async def on_ready():
                     cur.execute("INSERT INTO redditpostalerts (post_id) VALUES ('" + p.id + "');")
                     # In order to make the changes to the database permanent, we now commit our changes
                     conn.commit()
-                    await channel.send("@Turnips " + p.title + "\n" + p.url)
+                    await channel.send("<@707108605551312936> " + p.title + "\n" + p.url)
                 #await channel.send(p.url)
         # We have committed the necessary changes and can now close out our connection
         cur.close()
